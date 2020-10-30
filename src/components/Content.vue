9b5f0dee7c31cedae6c745baca9175c9d93c4754
Loyola <juanloyola@hotmail.com>
2020-10-29 22:06:46 -0300

    desktop done

---
+++ src/components/Content.vue
@@ -0,0 +1,268 @@
+<template>
+  <div class="container">
+    <section>
+        <aside>
+        <div>
+          <img src="@/assets/img/shoe.png" alt="shoe">
+        </div>
+
+        <footer>
+          <img src="@/assets/img/shoe.png">
+          <img src="@/assets/img/shoe top.png">
+          <img src="@/assets/img/shoeback.webp">
+          <img src="@/assets/img/shoeback3.png">
+        </footer>
+      </aside>
+
+      <aside>
+        <main>
+          <h1>{{model}}</h1>
+          <h1>{{price}}</h1>
+
+          <div>
+            <div class="table">
+              <p class="stock">US7</p>
+              <p class="stockOff">US8</p>
+              <p class="stock">US9</p>
+              <p class="active">US10</p>
+              <p class="stock">US11</p>
+              <p class="stockOff">US12</p>
+              <p class="table-add">
+              ADD <i class="fas fa-plus"/>
+            </p>
+            </div>
+          </div>
+
+        </main>
+      </aside>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  data () {
+    return {
+      model: 'Puma Rs-x Toys - Woman Shoes',
+      price: '$169.00'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+  background-color: #FCF6F3;
+  height: 100%;
+  section{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0rem 0 0 5rem;
+    overflow:hidden;
+  }
+
+  section > aside{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 45%;
+    height: auto;
+  }
+  section > aside > div{
+    border-radius: 50%;
+    height: 500px;
+    width: 500px;
+    background: #CA2728;
+    display: grid;
+    place-items: center ;
+  }
+
+  section > aside > div img{
+    width: 650px;
+    transform: translate(-80px,-100px);
+    filter: drop-shadow(0 40px 20px  rgba(0,0,0,0.5));
+    transition: .7s cubic-bezier(0.34, 1.56, 0.64, 1);
+    filter: brightness(125%);
+    cursor: pointer;
+  }
+  section > aside > footer img{
+    width: 50px;
+    margin: 10px 10px;
+  }
+  section > aside > div img:hover{
+    transform: rotateZ(27deg)  translate(-120px,-70px);
+  }
+
+  section > aside > main > h1{
+    font-size: 3rem;
+    text-align: start;
+    letter-spacing: 2px;
+  }
+
+  section > aside > main > div{
+    width: 100%;
+    margin: 30px 0;
+  }
+
+  .table{
+    display:flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: 1.3rem;
+    font-weight: bold;
+
+    &-add{
+      background-color:#CA2728;
+      border-radius: 9px;
+      color: #fff;
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      font-weight: normal;
+      margin: 0 20px;
+      height: 120px;
+      width: 90px;
+    }
+    .table i{
+      color:#FCF6F3;
+      margin: 0 10px;
+    }
+    i{
+      margin: 0 5px;
+    }
+    &-add:hover{
+      background-color:hsl(360, 68%, 57%);
+      cursor: pointer;
+    }
+  }
+  .table p {
+    margin: 0 10px;
+    cursor: pointer;
+  }
+  .active{
+    color: #DB644F;
+  }
+  .stock{
+    color: #636261;
+  }
+  .stockOff{
+    color: #B7ABAA;
+  }
+  .active, .stock:hover{
+    color: #da5037;
+  }
+}
+@media (max-width: 767px) {
+  .container{
+  background-color: #FCF6F3;
+  height: 100%;
+  section{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0rem 0 0 5rem;
+    overflow:hidden;
+  }
+
+  section > aside{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+    height: auto;
+  }
+  section > aside > div{
+    border-radius: 50%;
+    height: 250px;
+    width: 250px;
+    background: #CA2728;
+    display: grid;
+    place-items: center ;
+  }
+
+  section > aside > div img{
+    width: 250px;
+    transform: translate(0px,-20px);
+    filter: drop-shadow(0 40px 20px  rgba(0,0,0,0.5));
+    transition: .7s cubic-bezier(0.34, 1.56, 0.64, 1);
+    filter: brightness(125%);
+    cursor: pointer;
+  }
+  section > aside > footer{
+    flex-direction: row;
+    display:flex;
+  }
+
+  section > aside > footer img{
+    width: 50px;
+    margin: 5px 6px;
+  }
+  section > aside > div img:hover{
+    transform: rotateZ(27deg)  translate(-120px,-70px);
+  }
+
+  section > aside > main > h1{
+    font-size: 1rem;
+    text-align: start;
+    letter-spacing: 2px;
+  }
+
+  section > aside > main > div{
+    width: 50%;
+    margin: 30px 0;
+  }
+
+  .table{
+    display:flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: 1.3rem;
+    font-weight: bold;
+
+    &-add{
+      background-color:#CA2728;
+      border-radius: 9px;
+      color: #fff;
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      font-weight: normal;
+      margin: 0 20px;
+      height: 120px;
+      width: 90px;
+    }
+    .table i{
+      color:#FCF6F3;
+      margin: 0 10px;
+    }
+    i{
+      margin: 0 5px;
+    }
+    &-add:hover{
+      background-color:hsl(360, 68%, 57%);
+      cursor: pointer;
+    }
+  }
+  .table p {
+    margin: 0 10px;
+    cursor: pointer;
+  }
+  .active{
+    color: #DB644F;
+  }
+  .stock{
+    color: #636261;
+  }
+  .stockOff{
+    color: #B7ABAA;
+  }
+  .active, .stock:hover{
+    color: #da5037;
+  }
+}
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="container">
+    <div id="nav">
+      <div>
+        <input type="checkbox" id="click">
+      <label for="click" class="menu-btn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <ul>
+          <li><a href="#">New</a></li>
+          <li><a href="#">Man</a></li>
+          <li><a href="#">Woman</a></li>
+        <li><a class="logo" href="#"><img src="../assets/img/puma-logo.svg" alt="puma logo"></a></li>
+        <li><a href="#">SEARCH</a></li>
+        <li><a class="bag" href="#">BAG: 3</a></li>
+        <li class="userIcon"><img src="../assets/img/user.png" alt="user-icon">
+        <i class="fas fa-sort-down"></i></li>
+      </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+
+<style lang="scss" scoped>
+*{
+  margin: 0;
+  padding: 0px;
+}
+.container{
+
+  #nav {
+  display: flex;
+  height: 80px;
+  width: 100%;
+  background: #ffffff;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  z-index: 2;
+
+  font-family: 'Poppins';
+  color:#000;
+  position: sticky;
+  top: 0;
+}
+#nav ul{
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  justify-content: center;
+  align-items: center;
+}
+
+#nav ul li{
+  margin: 0 20px;
+
+  .logo img{
+    width: 105px;
+  }
+
+}
+  .userIcon i{
+    color: black;
+    visibility: visible;
+    display:flex;
+    align-items: center;
+    cursor: pointer;
+  }
+  .userIcon{
+    width: 2px;
+    display:flex;
+    flex-direction: row;
+  }
+  .userIcon img{
+    width: 50px;
+    border-radius: 50%;
+    margin: 0 20px;
+  }
+  #nav ul li a{
+    color: #000;
+    text-decoration: none;
+    font-size: 1rem;
+    font-weight: 500;
+    padding: 8px 0.3rem;
+    border-radius: 5px;
+    letter-spacing: 1px;
+    transition: all 0.3s ease;
+  }
+  #nav ul li a:hover{
+    color:#7389E1;
+  }
+  .bag {
+    background:#35353C;
+    color: #fff !important; // find the why i need !important
+    height: 70px;
+    width:80px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .bag:hover {
+    background-color: #525257;
+    color: #7389E1 !important; // find the why i need !important
+  }
+  #nav .menu-btn i{
+    color: #333;
+    font-size: 22px;
+    padding: 0 30px;
+    cursor: pointer;
+    display:none;
+  }
+
+  input[type="checkbox"] {
+    display: none;
+  }
+  @media (max-width: 1000px){
+    #nav{
+      padding: 0 40px 0 110px;
+    }
+  }
+  @media (max-width: 920px) {
+    #nav .menu-btn i{
+      display: block;
+      color :#35353C;
+    }
+    #click:checked ~ .menu-btn i:before{
+      content: "\f00d";
+    }
+    #nav ul{
+      position: fixed;
+      top: 80px;
+      left: -100%;
+      background: rgb(192, 191, 191);
+      height: 117vh;
+      width: 100%;
+      text-align: center;
+      display: block;
+      transition: all 0.3s ease;
+    }
+    #click:checked ~ ul{
+      left: 0;
+    }
+    #nav ul li{
+      width: 100%;
+      margin: 40px 0;
+    }
+    #nav ul li a{
+      width: 100%;
+      margin-left: -100%;
+      display: block;
+      font-size: 20px;
+      transition: 0.6s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+    }
+    #click:checked ~ ul li a{
+      margin-left: 0px;
+    }
+    #nav ul li a.active,
+    #nav ul li a:hover{
+      background: none;
+      color: cyan;
+    }
+      .bag {
+      background: none;
+      color: #fff !important;
+      height: 30px;
+    }
+  }
+}
+</style>
